9da3935b2d46cd6862e12a01c8d228a6debb2ec8
Nitesh Singh Jaswal <nitesh.jaswal@signify.com>
2023-03-24 01:43:25 +0100

    Added tests

---
+++ tests/test_api_models_dev_models_promptbuffer.py
@@ -0,0 +1,87 @@
+import pytest
+import pytest
+from api.models import Role, Prompt, PromptBuffer
+from api.exceptions import BufferExceeded
+from typing import List
+
+def test_prompt_buffer_append_within_limit():
+    buffer = PromptBuffer(2)
+    prompt1 = Prompt(Role.SYSTEM, "Hello")
+    prompt2 = Prompt(Role.USER, "Hi")
+
+    buffer.append(prompt1)
+    buffer.append(prompt2)
+
+    assert len(buffer) == 2
+    assert buffer[0] == prompt1
+    assert buffer[1] == prompt2
+
+def test_prompt_buffer_append_exceed_limit():
+    # Test raises when more than max_prompts
+    # Arrange
+    buffer = PromptBuffer(1)
+    prompt1 = Prompt(Role.SYSTEM, "Hello")
+    prompt2 = Prompt(Role.USER, "Hi")
+    buffer.append(prompt1)
+    # Act, Assert
+    with pytest.raises(BufferExceeded) as exc:
+        buffer.append(prompt2)
+    msg = exc.value.args
+    assert len(msg) == 1
+    assert msg[0] == f"Buffer exceeded the maximum number of prompts: 1"
+
+def test_prompt_buffer_extend_within_limit():
+    # Arrange
+    buffer = PromptBuffer(3)
+    prompts: List[Prompt] = [
+        Prompt(Role.SYSTEM, "Hello"),
+        Prompt(Role.USER, "Hi"),
+        Prompt(Role.SYSTEM, "How are you?")
+    ]
+
+    # Act
+    buffer.extend(prompts)
+    # Assert
+    assert len(buffer) == 3
+    for i, prompt in enumerate(prompts):
+        assert buffer[i] == prompt
+    prompts_in_buffer = []
+    for prompt in prompts:
+        prompts_in_buffer.append(prompt)
+    assert prompts == prompts_in_buffer
+
+def test_prompt_buffer_extend_exceed_limit():
+    # Arrange
+    buffer = PromptBuffer(2)
+    prompts: List[Prompt] = [
+        Prompt(Role.SYSTEM, "Hello"),
+        Prompt(Role.USER, "Hi"),
+        Prompt(Role.SYSTEM, "How are you?")
+    ]
+
+    # Act,Assert
+    with pytest.raises(BufferExceeded) as exc:
+        buffer.extend(prompts)
+    msg = exc.value.args
+    assert len(msg) == 1
+    assert msg[0] == f"Buffer exceeded the maximum number of prompts: 2"
+    
+
+def test_prompt_buffer_iter():
+    # Arrange
+    buffer = PromptBuffer(2)
+    prompt1 = Prompt(Role.SYSTEM, "Hello")
+    prompt2 = Prompt(Role.USER, "Hi")
+    # Act
+    buffer.append(prompt1)
+    buffer.append(prompt2)
+    for i, prompt in enumerate(buffer):
+        # Assert
+        assert prompt == buffer[i]
+
+def test_negative_max_prompts():
+    # Arrange, Act, Assert
+    with pytest.raises(ValueError):
+        PromptBuffer(-1)
+
+
--- tests/test_api_service___chatgpt_wrapper.py
+++ tests/test_api_service___chatgpt_wrapper.py
@@ -1,7 +1,76 @@
 import pytest
-import os
-from api.services import ChatGPT
+from api.services import ChatGPTClient
+from api.settings import OpenAIAuthSettings, OpenAIAPISettings
+from api.exceptions import OpenAIClientAuthException
+from api.models import Role, Prompt, PromptBuffer
+
+@pytest.fixture
+def mock_auth_settings() -> OpenAIAuthSettings:
+    return OpenAIAuthSettings(
+        api_secret="Th!5_iS_@_s3cREt", # type: ignore
+        organization_id="sample_organization_id"
+    )
+
+@pytest.fixture
+def mock_api_settings() -> OpenAIAPISettings:
+    return OpenAIAPISettings(max_prompts=10) # type: ignore
+
+def test_client_raises_auth_is_none(mock_api_settings: OpenAIAPISettings):
+    # Arrange, Act, Assert
+    with pytest.raises(OpenAIClientAuthException) as exc:
+        gpt_client = ChatGPTClient(api_settings=mock_api_settings)
+    msg = exc.value.args
+    assert len(msg) == 1
+    assert msg[0] == "Client auth settings not available. Please verify"
+
+def test_client_default_system_prompt(mock_auth_settings: OpenAIAuthSettings, mock_api_settings: OpenAIAPISettings):
+    # Arrange
+    ChatGPTClient.auth = mock_auth_settings
+    default_prompt = Prompt(role=Role.SYSTEM, content="Please answer in less than 200 words the response to the following query")
+    # Act
+    gpt_client = ChatGPTClient(api_settings=mock_api_settings)
+    assert isinstance(gpt_client.system_prompt, Prompt)
+    assert gpt_client.system_prompt == default_prompt
+        
+
+def test_client_buffer_list(mock_auth_settings: OpenAIAuthSettings, mock_api_settings: OpenAIAPISettings):
+    # Arrange
+    ChatGPTClient.auth = mock_auth_settings
+    # Act
+    gpt_client = ChatGPTClient(api_settings=mock_api_settings)
+    # Assert
+    assert gpt_client.buffer_list == []
+
+    # Arrange
+    messages = [Prompt(Role.USER, prompt) for prompt in (f"test prompt {i}" for i in range(5))]
+    # Act
+    return_value = gpt_client.add_messages(messages=messages)
+    # Assert
+    assert isinstance(return_value, ChatGPTClient)
+    assert gpt_client.buffer_list == messages
+
+def test_client_add_messages(mock_auth_settings: OpenAIAuthSettings, mock_api_settings: OpenAIAPISettings):
+    # Arrange
+    messages = [Prompt(Role.USER, prompt) for prompt in (f"test prompt {i}" for i in range(5))]
+    ChatGPTClient.auth = mock_auth_settings
+    gpt_client = ChatGPTClient(api_settings=mock_api_settings)
+    # Assert
+    assert isinstance(gpt_client.system_prompt, Prompt)
+    # Act
+    _ = gpt_client.add_messages(messages=messages).add_messages([Prompt(Role.USER, "extended_prompt")])
+    # Assert
+    assert gpt_client.buffer_list[-1].content == "extended_prompt"
+
+def test_client_send_messages(mock_auth_settings: OpenAIAuthSettings, mock_api_settings: OpenAIAPISettings):
+    # Arrange
+    messages = [Prompt(Role.USER, prompt) for prompt in (f"test prompt {i}" for i in range(5))]
+    ChatGPTClient.auth = mock_auth_settings
+    gpt_client = ChatGPTClient(api_settings=mock_api_settings)
+
+    response = gpt_client.add_messages(messages=messages).send_messages()
+    assert response.is_ok()
+    assert response.content == "This is a sample response"
 
 
-def test_get_chat_reponse():
-    assert True
+
+
