0cf59378650968c20364995e6b448ad2c9ed75f6
Nitesh Singh Jaswal <nitesh.jaswal@signify.com>
2023-03-26 22:29:38 +0200

    Added test app in tide

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use tide::Request;
+
+async fn hello_world(req: Request<()>) -> tide::Result<String> {
+    Ok("Hello, world!".to_string())
+}
+
+#[async_std::main] 
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+
+    app.at("/").get(hello_world);
+
+    app.listen("0.0.0.0:8001").await?;
+
+    Ok(())
 }
