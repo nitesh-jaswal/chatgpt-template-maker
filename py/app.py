c126d0d69c3ed82aff4f5710bddbdf5fee808f6c
Ankit Mathur <val.mathur@gmail.com>
2023-04-07 01:34:27 -0400

    add helpful docker run command

--- py/app.py
+++ py/app.py
@@ -3,6 +3,12 @@ This script prompts the user for a question on the terminal and hits openai API 
 
     Example Usage:
     (Note: The response to the above question will differ based on whether the chosen model is 3.5 or 4.)
+    ```sh
+    docker run -it --rm \
+        --env OPENAI_API_KEY=<your-api-key> \
+        -p 5000:5000 \
+        babajikifauj/gpt-4:latest
+    ```
         User: I'm in my house. On the top of my chair in the living room is a coffee cup. Inside the coffee cup is a thimble. Inside the thimble is a single diamond. I move the chair to my bedroom. Then I put the coffee cup on the bed. Then I turn the the cup upside down. Then I return it to rightside-up, and place the coffee cup on the kitchen counter. Where is my diamond?
 
         Bot (gpt-3.5-turbo): The diamond is inside the thimble, which is inside the coffee cup, which is on the kitchen counter.
